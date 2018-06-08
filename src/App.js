398118a8a37608cabfb83d0e52fb9ca88b9e977c
Kevin Kelly <kvinklly@gmail.com>
2018-06-08 17:35:56 -0500

    default home page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App" />
+      <div>
+        <h1>Game of Life</h1>
+        <p>Using react, redux & css grid</p>
+        <p>See code at <a href="https://github.com/mullaney/react-game-of-life">github</a>.</p>
+      </div>
     );
   }
 }
