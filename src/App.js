94a099f120c61257b07061ca0685e5988db07c68
Kevin Kelly <kvinklly@gmail.com>
2018-06-08 20:30:38 -0500

    eslint added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Game of Life</h1>
-        <p>Using react, redux & css grid!</p>
-        <p>See code at <a href="https://github.com/mullaney/react-game-of-life">github</a>.</p>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <h1>Game of Life</h1>
+      <p>Using react, redux & css grid!</p>
+      <p>See code at <a href="https://github.com/mullaney/react-game-of-life">github</a>.</p>
+    </div>
+  );
+};
 
 export default App;
