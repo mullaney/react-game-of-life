1c6f1ea8c766953de7c27ddc4f9efc22485d13e0
Kevin Kelly <kvinklly@gmail.com>
2018-06-11 17:50:10 -0500

    start stop working

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import Grid from './components/Grid';
 import Cell from './components/Cell';
@@ -8,45 +8,71 @@ import Header1 from './components/Header1';
 import Text from './components/Text';
 import Button from './components/Button';
 import { connect } from 'react-redux';
-import { clearGrid, randomizeGrid, oneTick } from './store';
+import { clearGrid, randomizeGrid, oneTick, togglePlay } from './store';
 
-export const App = props => {
-  const {
-    rows, cols, cells, size,
-    handleClear,
-    handleRandom,
-    handleTick
-  } = props;
+export class App extends Component {
+  constructor() {
+    super();
+    this.tickId = null;
+  }
 
-  let counter = 0;
+  componentDidUpdate(prevProps) {
+    console.log('this.props.running: ', this.props.running);
+    if (prevProps.running !== this.props.running) {
+      if (this.props.running) {
+        this.tickId = setInterval(this.props.handleTick, 5);
+        console.log('this.props.handleTick: ', this.props.handleTick);
+        console.log('this.tickId: ', this.tickId);
+      } else {
+        window.clearInterval(this.tickId);
+        this.tickId = null;
+        console.log('this.tickId: ', this.tickId);
+      }
+    }
+  }
 
-  return (
-    <Row>
-      <Column>
-        <Header1>Game of Life</Header1>
-        <Text>Using react, redux, styled components & css grid! See code at <a href="https://github.com/mullaney/react-game-of-life">github</a>.</Text>
-        <Grid cols={cols} rows={rows} size={size}>
-          {cells && cells.map(cell => {
-            return <Cell alive={!!cell} key={counter++} />;
-          })}
-        </Grid>
-        <Row>
-          <Button onClick={handleTick} >></Button>
-          <Button>Play</Button>
-          <Button onClick={handleClear} >Clear</Button>
-          <Button onClick={handleRandom}>Random</Button>
-        </Row>
-      </Column>
-    </Row>
-  );
-};
+  render() {
+    const {
+      rows, cols, cells, size, running,
+      handleClear,
+      handleRandom,
+      handleTick,
+      handlePlayStop,
+    } = this.props;
+
+    let counter = 0;
+
+
+    return (
+      <Row>
+        <Column>
+          <Header1>Game of Life</Header1>
+          <Text>Using react, redux, styled components & css grid! See code at <a href="https://github.com/mullaney/react-game-of-life">github</a>.</Text>
+          <Grid cols={cols} rows={rows} size={size}>
+            {cells && cells.map(cell => {
+              return <Cell alive={!!cell} key={counter++} />;
+            })}
+          </Grid>
+          <Row>
+            <Button onClick={handleTick} >></Button>
+            <Button onClick={handlePlayStop}>{running ? 'Stop' : 'Play'}</Button>
+            <Button onClick={handleClear} >Clear</Button>
+            <Button onClick={handleRandom}>Random</Button>
+          </Row>
+        </Column>
+      </Row>
+    );
+  }
+}
 
 const mapState = (state) => {
   console.log('state: ', state);
   return {
     rows: state.game.rows,
     cols: state.game.cols,
-    cells: state.game.cells
+    cells: state.game.cells,
+    running: state.game.running,
+    size: state.game.size
   };
 };
 
@@ -60,6 +86,9 @@ const mapDispatch = (dispatch) => {
     },
     handleTick() {
       dispatch(oneTick());
+    },
+    handlePlayStop() {
+      dispatch(togglePlay());
     }
   };
 };
--- src/store/game.js
+++ src/store/game.js
@@ -1,15 +1,18 @@
 import {randomGrid, tick} from './helpers';
 
-const DEFAULT_SIZE = 60;
+const DEFAULT_SIZE = 40;
 
 export const CLEAR_GRID = 'CLEAR_GRID';
 export const RANDOMIZE_GRID = 'RANDOMIZE_GRID';
 export const ONE_TICK = 'ONE_TICK';
+export const TOGGLE_PLAY = 'TOGGLE_PLAY';
 
 export const initialState = {
   cells: new Array(DEFAULT_SIZE * DEFAULT_SIZE).fill(0),
   rows: DEFAULT_SIZE,
-  cols: DEFAULT_SIZE
+  cols: DEFAULT_SIZE,
+  running: false,
+  size: '15px'
 };
 
 // Action Creators
@@ -17,13 +20,17 @@ export const initialState = {
 export const clearGrid = () => ({ type: CLEAR_GRID });
 export const randomizeGrid = () => ({ type: RANDOMIZE_GRID });
 export const oneTick = () => ({ type: ONE_TICK });
+export const togglePlay = () => ({ type: TOGGLE_PLAY });
 
 // Reducer
 
 export default function (state = initialState, action) {
-  const {rows, cols, cells} = state;
+  const {rows, cols, cells, running} = state;
 
   switch (action.type) {
+    case TOGGLE_PLAY:
+      return {...state, running: !running };
+
     case ONE_TICK:
       return {...state, cells: tick(cells, [rows, cols])};
 
