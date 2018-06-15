37ea0450ae94827c3bc667046186275f1c6a4cd3
Kevin Kelly <kvinklly@gmail.com>
2018-06-14 23:14:15 -0500

    bug fix and title update

--- src/store/game.js
+++ src/store/game.js
@@ -47,7 +47,7 @@ export default function (state = initialState, action) {
       return {...state, cells: tick(cells, [rows, cols])};
 
     case CLEAR_GRID:
-      return {...state, cells: [...cells].fill(0)};
+      return {...state, cells: cells.fill(0), running: false };
 
     case RANDOMIZE_GRID:
       return {...state, cells: randomGrid(rows * cols)};
